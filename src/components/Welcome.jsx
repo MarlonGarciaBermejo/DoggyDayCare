c65ed4201f1f14d8e88e8e668e63b67d682e62e3
Marlon <marlongarciab@hotmail.com>
2023-10-05 16:05:16 +0200

    final

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,7 +5,6 @@ const Welcome = (props) => {
 return (
 <section className='content'>
 <h1 className='h11'>Welcome to <img className="img" src="/animal-icon-png-6266.png" width="85" alt="img"/><span className='span1'>DoggyDayCare.</span></h1>
-<h3 className='h22'>We care for your loved ones.</h3>
 </section>
 
 )
