023e9817f1cfa17ebbb848d3479fd2d98766b845
Marlon <marlongarciab@hotmail.com>
2023-10-06 15:30:35 +0200

    fixed sizing

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,18 @@
 import './Welcome.css';
 
-const Welcome = (props) => {
+const Welcome = () => {
 
 return (
 <section className='content'>
 <h1 className='h11'>Welcome to <img className="img" src="/animal-icon-png-6266.png" width="85" alt="img"/><span className='span1'>DoggyDayCare.</span></h1>
+<p>We understand that your furry family members are more than just pets; they are cherished companions who deserve the very best care and attention. 
+That's why we've created a home away from home, a place where your four-legged friends can play, learn, and socialize in a safe and loving environment.
+Our dedicated team of experienced and passionate caregivers is here to ensure that your dogs have a tail-wagging good time.
+ From the moment they step through our doors, they'll be greeted with smiles and open arms. 
+ We're committed to making their time with us enjoyable, comfortable, and filled with exciting activities. </p>
+<br /> <br /> 
 </section>
-
 )
 }
+
 export default Welcome; 
